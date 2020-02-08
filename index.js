bc6e0a22ea07ba87d0dd8a158b038cdfe351a6b9
AIT AHTMAN Salah Eddine <aitahtman.salaheddine@gmail.com>
2020-02-08 12:46:55 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
 app.listen(port, function () {
-    console.log(`server is ready 🔥🔥🔥 ${port}`)
+    console.log(`server is ready 🔥 🔥 🔥 ${port}`)
 })
